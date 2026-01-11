c2aa7af62c3d9133a7444f2914c5cb6e36e93001
aleksajaw <jaworska.aleksa@gmail.com>
2026-01-11 18:40:29 +0100

    Add Player class

--- script.js
+++ script.js
@@ -108,6 +108,48 @@ class GameState {
 
 
 
+class Player {
+    constructor ( id = null, mark = null, name = null, isBotModeActive = false ) {
+        this.id = id;
+        this.mark = mark;
+        this.name = name;
+        this.isBotModeActive = isBotModeActive;
+        this.botModeDetails = { name: 'computer',
+                                defaultName: name };
+    }
+    setName ( name = '') {
+        this.name = name
+    }
+    isDefault () {
+        return this.id === null
+            && this.mark === null
+            && this.name === null
+    }
+    isBot () {
+        return this.isBotModeActive
+    }
+    toggleBotMode () {
+        this.isBotModeActive = !this.isBotModeActive
+        let newName = this.isBotModeActive ? this.botModeDetails.defaultName
+                                           : this.name
+        this.setName( newName );
+    }
+    isPropertyEqualTo ( name = 'id', value = null) {
+        return this[name] === value
+    }
+    isPropertyNotEqualTo ( name = 'id', value = null) {
+        return this[name] !== value
+    }
+    matchesId ( anotherPlayer ) {
+        if (anotherPlayer instanceof Player)
+            return null;
+        
+        return this.id === anotherPlayer.id;
+    }
+}
+
+
+
 class BoardState {
     constructor ( matrixState = [ [ '', '', '' ], [ '', '', '' ], [ '', '', '' ] ] ) {
         this.matrixState = matrixState;
